7dbbaed0f56c4cd580dad865e54367c01bfca248
gustavo867 <tartatavo3@gmail.com>
2021-04-16 20:53:50 -0300

    :tada: finished transaction

--- lib/rocketpay_web/views/accounts_view.ex
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -1,5 +1,6 @@
 defmodule RocketpayWeb.AccountsView do
   alias Rocketpay.Account
+  alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
 
   def render("update.json", %{
       account: %Account{
@@ -15,4 +16,25 @@ defmodule RocketpayWeb.AccountsView do
       }
     }
   end
+
+  def render("transaction.json", %{
+    transaction: %TransactionResponse{
+      to_account: to_account,
+      from_account: from_account,
+    },
+  }) do
+  %{
+    message: "Transaction done successfully",
+    transaction: %{
+      from_account: %{
+          id: from_account.id,
+          balance: from_account.balance,
+      },
+      to_account: %{
+        id: to_account.id,
+        balance: to_account.balance,
+      }
+    }
+  }
+end
 end
